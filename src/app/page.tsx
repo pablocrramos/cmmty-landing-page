5f47283da8d726f4ee976801842ed700ed0f9bc8
jpablocruz <pablocrzrms@gmail.com>
2026-04-01 16:36:19 -0600

    leaving this section for later as it is not key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <ServicesShowcase />
         <FactsBento />
         <HowItWorks />
-        <WhyUs />
+        {/* <WhyUs /> */}
         <CtaBanner />
       </main>
       <Footer />
